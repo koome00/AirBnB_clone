00c593fd5ca35e7bf4423ff1305d67c06538df93
koome <koomemc@gmail.com>
2023-10-13 04:26:17 +0300

    update console

--- console.py
+++ console.py
@@ -32,6 +32,7 @@ class HBNBCommand(cmd.Cmd):
             return
         
         args = line.split(" ")        
+        
         for key in all_classes.keys():
             if args[0] != key:
                 print("** class doesn't exist **")
@@ -45,23 +46,26 @@ class HBNBCommand(cmd.Cmd):
         Prints the string representation
         of an instance based on the class name and id
         """
+        args = line.split(" ")
         if len(line) == 0:
             print("** class name missing **")
             return
-        
-        args = line.split(" ")
-        if not args[1]:
-                print("** instance id missing **")
-        else:
-            instance = "{}.{}".format(args[0], args[1])
-            for key, values in storage.all().items():
-                if key == instance:
-                    print(values)
-            if instance not in storage.all().keys():
-                print("** no instance found **")
             
         if args[0] not in storage.all_classes.keys():
             print("** class doesn't exist **")
+        else:
+            if len(args) == 1:
+                print("** instance id missing **")
+
+            if len(args) == 2:
+                instance = "{}.{}".format(args[0], args[1])
+                for key, values in storage.all().items():
+                    if key == instance:
+                        print(values)
+            
+                if instance not in storage.all().keys():
+                    print("** no instance found **")
+
     
     def do_destroy(self, line):
         """
@@ -80,6 +84,7 @@ class HBNBCommand(cmd.Cmd):
             print("** class doesn't exist **")
 
         instance="{}.{}".format(args[0], args[1])
+        
         if instance in storage.all().keys():
             del storage.all()[instance]
             storage.save()
@@ -94,7 +99,6 @@ class HBNBCommand(cmd.Cmd):
         inst_list = []
         args = line.split(" ")
 
-        
         if len(line) == 0:
             for item in storage.all().values():
                 inst_list.append(str(item))
@@ -110,7 +114,55 @@ class HBNBCommand(cmd.Cmd):
         else:
             print("** class doesn't exist **")
             return
-            
+        
+    def do_update(self, line):
+        """
+        Updates an instance based on
+        the class name and id by adding or updating attribute
+        """
+        args = line.split(" ")
+        
+        if len(line) == 0:
+            print("** class name missing **")
+            return
+        
+        args = line.split(" ")
+
+        if args[0] not in storage.all_classes.keys():
+            print("** class doesn't exist **")
+            return
+        else:
+            if len(args) == 1:
+                pass
+            if len(args) > 1:
+                instance="{}.{}".format(args[0], args[1])
+                if instance not in storage.all().keys():
+                    print("** no instance found **")
+                    return
+        
+        if len(args) == 1:
+            print("** instance id missing **")
+            return
+        
+        if len(args) == 2:
+            print("** attribute name missing **")
+            return
+        
+        if len(args) >= 4:
+            instance = "{}.{}".format(args[0], args[1])
+            cast = type(eval(args[3]))
+            arg3 = args[3]
+            arg3 = arg3.strip('"')
+            arg3 = arg3.strip("'")
+            setattr(storage.all()[instance], args[2], cast(arg3))
+            storage.save()
+        else:
+            print("** value missing **")
+        
+        
+        
+
+
         
         
 
