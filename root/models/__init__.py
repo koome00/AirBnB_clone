9720e873db2bda10d3ca958f5bad2b02688b8df5
koome <koomemc@gmail.com>
2023-10-15 21:59:46 +0300

    changes

--- root/models/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-""" Create a unique file storage for your application"""
-from models.engine.file_storage import FileStorage
-from models.base_model import BaseModel
-
-storage = FileStorage()
-storage.reload()
-all_classes = {"BaseModel": BaseModel}
--- root/models/state.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-""" state class that inherits from BaseModel"""
-from models.base_model import BaseModel
-
-
-class state(BaseModel):
-    """public attributes"""
-
-    name = ""
